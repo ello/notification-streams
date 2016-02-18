b61ff6b8e507ed452ac61f5cee63124413f78a6b
Jay Zeschin <jay.zeschin@modeset.com>
2016-02-18 11:22:52 -0700

    Refactor notification model specs to use shoulda-matchers

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -12,40 +12,20 @@ RSpec.describe Notification, type: :model do
   end
 
   describe 'validations' do
-    let(:notification) { Notification.new }
+    it { is_expected.to validate_presence_of(:user_id) }
+    it { is_expected.to validate_presence_of(:subject_id) }
+    it { is_expected.to validate_presence_of(:subject_type) }
+    it { is_expected.to validate_presence_of(:kind) }
 
-    it 'is invalid if user_id is blank' do
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['user_id']).to include("can't be blank")
+    it { is_expected.to allow_value('User').for(:subject_type) }
+    it { is_expected.to allow_value('Post').for(:subject_type) }
+    it { is_expected.to allow_value('Love').for(:subject_type) }
+    it { is_expected.not_to allow_value('Foo').for(:subject_type) }
+
+    Notification::NOTIFICATION_STREAM_KINDS.each do |kind|
+      it { is_expected.not_to allow_value(kind).for(:kind) }
     end
 
-    it 'is invalid if subject_id is blank' do
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['subject_id']).to include("can't be blank")
-    end
-
-    it 'is invalid if subject_type is blank' do
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['subject_type']).to include("can't be blank")
-    end
-
-    it 'is invalid if subject_type is not a valid type' do
-      notification.subject_type = 'Foo'
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['subject_type'])
-        .to include('is not included in the list')
-    end
-
-    it 'is invalid if kind is blank' do
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['kind']).to include("can't be blank")
-    end
-
-    it 'is invalid if kind is not a valid kind' do
-      notification.kind = 'foo_notification'
-      expect(notification.valid?).to be_falsy
-      expect(notification.errors['kind'])
-        .to include('is not included in the list')
-    end
+    it { is_expected.not_to allow_value('foo_notification').for(:kind) }
   end
 end
